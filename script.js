08bc35f6682b23f25e29a6255a322d4bfeb9e623
Alexandra <alexandrita@ukr.net>
2019-08-28 23:36:40 +0300

    Add

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var a = { x: 1, y: 2, z: [1, 2, 3], u: undefined, v: null, a: {a: 12, b: 13}};
+var a = { d: {c: {a: 2}}, x: 1, y: 2, z: [1, 2, 3], u: undefined, v: null, a: {a: 12, b: 13}};
 
 function deepCopy(a){
   let b = new Object();
@@ -8,6 +8,11 @@ function deepCopy(a){
     } 
     else if (value instanceof Object){
       b[key] = Object.defineProperties({}, Object.getOwnPropertyDescriptors(value));
+      for (let [key1, value1] of Object.entries(value)){
+        if(value1 instanceof Object) {
+          b[key][key1] = Object.defineProperties({}, Object.getOwnPropertyDescriptors(value1));
+        }
+      }     
     }
     else {
       b[key] = value;
@@ -15,6 +20,7 @@ function deepCopy(a){
   }
   return b;
 }
+
 
 var b = deepCopy(a);
 console.log(a);
@@ -31,3 +37,6 @@ b.a.a = 13;
 a.a.a = 5;
 console.log(a.a.a);
 console.log(b.a.a);
+b.d.c.a = 5;
+console.log(a.d.c.a);
+console.log(b.d.c.a);
